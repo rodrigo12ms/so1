b975dcc69c5784a0875a59c6da7e47c883ece694
rodrigo12ms <espinozasayagorodrigo@gmail.com>
2022-07-27 08:56:58 -0500

    primer commit

---
+++ Procesos/proceso.c
@@ -0,0 +1,12 @@
+
+/* Programa: Hola mundo (Versión 2) */
+
+#include <stdio.h>
+
+int main()
+{
+    printf( "\n   Hola mundo." );
+    printf( "\n\n   Pulse una tecla para salir..." );
+
+    return 0;
+}
